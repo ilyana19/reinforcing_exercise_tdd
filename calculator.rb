2af4dc82e35b463af729597382f04158e0d20f64
ilyana19 <aries_2152@hotmail.com>
2018-08-26 21:34:03 -0400

    finished calculator test exercise

---
+++ calculator.rb
@@ -0,0 +1,15 @@
+def add(n1, n2)
+  return n1 + n2
+end
+
+def subtract(n1, n2)
+  return n1 - n2
+end
+
+def sum(array)
+  counter = 0
+  array.each do |num|
+    counter += num
+  end
+  return counter
+end
